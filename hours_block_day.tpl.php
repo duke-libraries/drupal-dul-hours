24b1835d2a8c8cd28f14b6f656a8425542532263
Derrek Croney <derrek.croney@duke.edu>
2013-06-03 10:52:53 -0400

    include public hours, as needed, when rendering block view

--- hours_block_day.tpl.php
+++ hours_block_day.tpl.php
@@ -1,6 +1,11 @@
 <?php
 ?>
 <div class="hours_day">
-	<div class="day"><?php echo $item->libraryDate['dayAndDate']; ?></div>
-	<div class="hours"><?php echo $item->hours; ?></div>
+	<div class="dukeusers">
+		<div class="hours"><?php echo $item->hours; ?></div>
+		<span class="title"><?php echo $item->libraryDate['dayAndDate']; ?></span>
+	</div>
+	<?php if(!empty($public_item)): ?>
+	<div class="genpublic">Public: <?php echo $public_item->hours; ?></div>
+	<?php endif; ?>
 </div>
