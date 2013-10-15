3f7a12d7ba80328da0b84ecb51d91b68be0b2758
Derrek Croney <derrek.croney@duke.edu>
2013-10-15 10:01:43 -0400

    no msg

--- hours_block_day.tpl.php
+++ hours_block_day.tpl.php
@@ -2,8 +2,7 @@
 ?>
 <div class="hours-row<?php echo $item->isCurrentDay ? ' hours-today' : ''; ?>">
 	<span class="day"><?php echo $item->libraryDate['dayAndDate']; ?></span>
-	<span class="hours"><?php echo $item->hours_encoded; ?>
-
+	<span class="hours"><?php echo $item->hours_encoded; ?></span>
 	<?php if(!empty($public_item)): ?>
 	<div class="genpublic">Public: <?php echo $public_item->hours; ?></div>
 	<?php endif; ?>
