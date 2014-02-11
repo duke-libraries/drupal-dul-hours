4c4fb70d8f89af2f3cdfb064f5650dfc4ac4abc2
Derrek Croney <derrek.croney@duke.edu>
2014-02-11 17:26:28 -0500

    RT 261619

---
+++ affiliate_hours.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Empty template file that can contain links to affiliate libraries
+ * (Ford, Medical Center, etc)
+ *
+ * To customize, copy to your theme's directory and modify it.
+ */
+?>
+
+<!-- THIS IS FOR DEBUGGING -->
